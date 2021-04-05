31dd5bc2e25f5fc243f93c0f20708c4c495c41e2
annamac <fenicemail1@gmail.com>
2021-04-05 23:46:24 +0300

    add hw5

--- src/Homework/HW5/BankAccount.java
+++ src/Homework/HW5/BankAccount.java
@@ -1,4 +1,27 @@
 package Homework.HW5;
 
 public class BankAccount {
+
+    static double balance = 567.09;
+
+    static double popolnenieScheta(double a) {
+        return balance + a;
+
+    }
+
+    static double snyatieSoScheta(double b) {
+        return balance - b;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        double c = snyatieSoScheta(30);
+        System.out.println(c);
+        double n = popolnenieScheta(560.06);
+        System.out.println(n);
+
+    }
+
 }
--- src/Homework/HW5/Employee.java
+++ src/Homework/HW5/Employee.java
@@ -1,4 +1,24 @@
 package Homework.HW5;
 
 public class Employee {
+
+    int id;
+    int age;
+    double salary;
+    String surname;
+    String departament;
+
+    public Employee(int id, int age, double salary, String surname, String departament) {
+        this.id = id;
+        this.age = age;
+        this.salary = salary;
+        this.surname = surname;
+        this.departament = departament;
+    }
+
+    double uvelechenieZP(){
+        salary *= 2;
+        return salary;
+    }
+
 }
