46d98fd83e4e475a60c35ab61f44aff4b780518d
Anna Kizeeva <fenicemail1@gmail.com>
2022-03-28 23:03:49 +0300

    add_loopFor_task

---
+++ src/training/ForLoops.java
@@ -0,0 +1,26 @@
+package training;
+
+import java.util.Scanner;
+
+public class ForLoops {
+
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt(); // 1
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt(); //2
+            int b = in.nextInt(); //3
+            int n = in.nextInt(); //4
+
+            for (int j = 0; j < n; j++) {
+                a += b * Math.pow(2, j);
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+
+
+    }
+
+}
